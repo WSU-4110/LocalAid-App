6c5946561767b132d8934eb26729c544b1fc7a62
Bryan Akani <akabry25@gmail.com>
2025-07-28 21:06:24 -0400

    finalize unit test

--- app/routes/requests.py
+++ app/routes/requests.py
@@ -1,15 +1,33 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template_string, request, redirect, url_for, flash
 
 bp = Blueprint('requests', __name__)
 
+
+BROWSE_TEMPLATE = """
+{% with messages = get_flashed_messages(with_categories=true) %}
+  {% if messages %}
+    {% for category, message in messages %}
+      <div class="flash {{ category }}">{{ message }}</div>
+    {% endfor %}
+  {% endif %}
+{% endwith %}
+<h1>Browse Help Requests</h1>
+"""
+POST_TEMPLATE = """
+<form method='post'>
+    <input type='submit' value='Post Request'>
+</form>
+"""
+
 @bp.route('/browse')
 def browse():
-    return render_template('browse.html')
+    
+    return render_template_string(BROWSE_TEMPLATE)
 
 @bp.route('/post-request', methods=['GET', 'POST'])
 def post_request():
     if request.method == 'POST':
-        # Save help request logic here
+        
         flash('Your request has been posted!', 'success')
         return redirect(url_for('requests.browse'))
-    return render_template('post-request.html')
+    return render_template_string(POST_TEMPLATE)
